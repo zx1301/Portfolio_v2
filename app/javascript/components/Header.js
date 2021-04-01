7b9e49e688f5a12822e2c885622ddf7bb3991bb6
zx1301 <zx1301@gmail.com>
2021-03-31 21:44:58 -0400

    add background img

---
+++ app/javascript/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1>web dev</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
