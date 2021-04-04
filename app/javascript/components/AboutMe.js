86c8b0f9845b0f9e510d3719af87e8631c17eb54
zx1301 <zx1301@gmail.com>
2021-04-04 15:45:30 -0400

    add about me page

---
+++ app/javascript/components/AboutMe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import author from "../../assets/images/me3.jpg";
+
+const AboutMe = () => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-6 col-xm-12">
+          <div className="photo-wrap mb-5">
+            <img className="profile-img" src={author} alt="author..." />
+          </div>
+        </div>
+        <div className="col-lg-6 col-xm-12">
+          <h1 className="about-heading">about me</h1>
+          <p>
+            hello, I'm tony. 
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe
--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -5,7 +5,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+<nav className="navbar navbar-expand-lg navbar-light bg-secondary">
 <div className="container">
   <a className="navbar-brand" href="/">Anthony Dohyung Kim</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
